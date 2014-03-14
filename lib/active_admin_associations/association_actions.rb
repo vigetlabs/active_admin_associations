c9d042fe967084f0b85e9bd9fe0e0c49a7f0263e
Brian Landau <brianjlandau@gmail.com>
2014-03-14 13:19:34 -0600

    Improve ActiveAdminAssociations::AssociationActions organization

--- lib/active_admin_associations/association_actions.rb
+++ lib/active_admin_associations/association_actions.rb
@@ -1,40 +1,50 @@
 module ActiveAdminAssociations
   module AssociationActions
     def association_actions
+      controller do
+        include InstanceMethods
+      end
+
       member_action :unrelate, :method => :put do
-        reflection = resource_class.reflect_on_association(params[:relationship_name].to_sym)
-
-        if reflection.collection?
-          related_record = reflection.klass.find(params[:related_id])
-          resource.send(params[:relationship_name]).delete(related_record)
-        else
-          resource.update_attribute(reflection.foreign_key, nil)
-        end
-
-        flash[:notice] = "The recored has been unrelated."
-
-        redirect_to request.headers["Referer"].presence || admin_dashboard_url
+        unrelate_record
       end
 
       member_action :relate, :method => :put do
-        reflection = resource_class.reflect_on_association(params[:relationship_name].to_sym)
-
-        if reflection.collection?
-          record_to_relate = reflection.klass.find(params[:related_id])
-          resource.send(params[:relationship_name]) << record_to_relate
-        else
-          resource.update_attribute(reflection.foreign_key, params[:related_id])
-        end
-
-        flash[:notice] = "The recored has been related."
-
-        redirect_to request.headers["Referer"].presence || admin_dashboard_url
+        relate_record
       end
 
       member_action :page_related, :method => :get do
-        relationship_name = params[:relationship_name].to_sym
-        association_config = active_admin_config.form_associations[relationship_name]
-        relationship_class = resource_class.reflect_on_association(relationship_name).klass
+        page_related_resources
+      end
+    end
+
+    module InstanceMethods
+      private
+
+      def unrelate_record
+        if relationship_reflection.collection?
+          related_record = relationship_class.find(params[:related_id])
+          resource.send(relationship_name).delete(related_record)
+        else
+          resource.update_attribute(relationship_reflection.foreign_key, nil)
+        end
+
+        redirect_back_or_dashboard("The recored has been unrelated.")
+      end
+
+      def relate_record
+        if relationship_reflection.collection?
+          record_to_relate = relationship_class.find(params[:related_id])
+          resource.send(relationship_name) << record_to_relate
+        else
+          resource.update_attribute(relationship_reflection.foreign_key, params[:related_id])
+        end
+
+        redirect_back_or_dashboard("The recored has been related.")
+      end
+
+      def page_related_resources
+        association_config  = active_admin_config.form_associations[relationship_name]
         association_columns = association_config.fields.presence || relationship_class.content_columns
         
         render :partial => 'admin/shared/collection_table', :locals => {
@@ -44,6 +54,23 @@ module ActiveAdminAssociations
           :columns            => association_columns,
           :relationship_class => relationship_class
         }
+      end
+
+      def relationship_name
+        params[:relationship_name].to_sym
+      end
+
+      def relationship_reflection
+        resource_class.reflect_on_association(relationship_name)
+      end
+
+      def relationship_class
+        relationship_reflection.klass
+      end
+
+      def redirect_back_or_dashboard(flash_message)
+        flash[:notice] = flash_message
+        redirect_to request.headers["Referer"].presence || admin_dashboard_url
       end
     end
   end
