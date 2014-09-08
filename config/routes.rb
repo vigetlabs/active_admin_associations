e4e22a76053b31e2fa5a50bd618880843ac09ece
Brian Landau <brianjlandau@gmail.com>
2014-09-08 11:39:32 -0600

    Prepend routes instead of appending them

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 if Rails.application.config.activeadmin_associations.autocomplete_models
   models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
-  Rails.application.routes.draw do
+  Rails.application.routes.prepend do
     match '/autocomplete/:model' => 'autocomplete#index', :model => /(#{models})/,
       :defaults => { :format => 'json' }
   end
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  FOUR_O_FOUR_ERRORS = [
+    ActiveRecord::RecordNotFound,
+    ActionView::MissingTemplate,
+    ActionController::RoutingError,
+    ActionController::MethodNotAllowed,
+    ActionController::NotImplemented,
+    AbstractController::ActionNotFound,
+    ActionController::UnknownController,
+    ActionController::UnknownHttpMethod
+  ]
+
+  rescue_from *FOUR_O_FOUR_ERRORS, :with => :render_404
+
+  def render_404
+    render :template => 'shared/404', :formats => [:html], :status => 404
+  end
 end
